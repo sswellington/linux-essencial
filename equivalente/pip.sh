e6f0b36edd1319b90f9723f146b1bb15503ef7b3
Wellington Silva <wellingtondessilva@gmail.com>
2020-04-24 18:08:23 -0300

    feat: Kivy e Pillow

--- equivalente/pip.sh
+++ equivalente/pip.sh
@@ -3,4 +3,9 @@
 # sudo chmod 777 pip.sh
 # sudo ./pip.sh
 
+# [jupyterlab](https://jupyterlab.readthedocs.io/en/stable/getting_started/installation.html)
 sudo pip install jupyterlab
+# [interface](https://kivy.org/doc/stable/guide/basic.html#quickstart)
+pip install kivy
+# [Python Imaging Library](https://pillow.readthedocs.io/en/stable/)
+pip install Pillow
