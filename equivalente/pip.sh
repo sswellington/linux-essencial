131c49e881462009024082eb99b3c97a0838856d
Wellington Silva <wellingtondessilva@gmail.com>
2020-05-18 15:18:21 -0300

    feat: lektor

--- equivalente/pip.sh
+++ equivalente/pip.sh
@@ -3,6 +3,11 @@
 # sudo chmod 777 pip.sh
 # sudo ./pip.sh
 
+# Remove todos os pacotes do pip
+pip freeze | xargs pip uninstall -y
+# or 
+pip freeze | grep -v "^-e" | xargs pip uninstall -y
+
 # [virtualenv](https://pythonacademy.com.br/blog/python-e-virtualenv-como-programar-em-ambientes-virtuais)
 pip install virtualenv
 # [jupyterlab](https://jupyterlab.readthedocs.io/en/stable/getting_started/installation.html)
@@ -13,3 +18,5 @@ pip install kivy
 pip install Pillow
 # [Construção de página WEB](https://flask.palletsprojects.com/)
 pip install Flask
+# [Construção de página estático](https://www.getlektor.com/)
+pip install lektor
